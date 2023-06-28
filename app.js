f2b53e7d1c5eeb78f418970936c6e4daf97ed2f7
Isaac N. Reis <isaacnas.arts@gmail.com>
2023-06-28 16:28:03 -0300

    adjusting function scope

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { Db } from "./js/Class/Db.js";
 import { Expense } from "./js/Class/Expense.js";
 
-import { showModal, carriesExpenses } from "./js/functions/function.js";
+import { showModal } from "./js/functions/function.js";
 
 const db = new Db();
 
@@ -62,7 +62,7 @@ if(location.href == 'https://isaacnreis.github.io/orcamento-pessoal/index.html' 
 if(location.href == 'https://isaacnreis.github.io/orcamento-pessoal/consulta.html'){
 
   const body = document.querySelector('body');
-  body.addEventListener('load', carriesExpenses( undefined, undefined, db))
+  body.addEventListener('load', carriesExpenses())
 
   const searchForm = document.getElementById('searchForm');
 
@@ -87,8 +87,66 @@ if(location.href == 'https://isaacnreis.github.io/orcamento-pessoal/consulta.htm
   
     let expenses = db.search(expense)
   
-    carriesExpenses(expenses, true, db);
+    carriesExpenses(expenses, true);
   
   })
 
 }
+
+function carriesExpenses(expenses = [], filter = false){
+
+  if(expenses.length == 0 && filter == false){
+    // Recuperando um array com todas as despesas registradas
+    expenses = db.recoverAllData();
+  }
+
+  // Elemento tbody html
+  const expenseRecord = document.getElementById('expenseRecord');
+  expenseRecord.innerHTML = ''; 
+
+  // Passando por cada elemento do array de despesas
+  // Colocando no hmtl em seus respectivos campos
+  expenses.forEach(expense => {
+    
+    // Inserindo tr
+    let line = expenseRecord.insertRow();
+
+    // Inserindo th
+    line.insertCell(0).innerHTML = `${expense.day}/${expense.month}/${expense.year}`; //Data
+
+    // Ajustando valor do tipo da despesa
+    switch(expense.type){
+      case '1': expense.type = 'Alimentção'
+        break
+      case '2': expense.type = 'Educação'
+        break
+      case '3': expense.type = 'Lazer'
+        break
+      case '4': expense.type = 'Saúde'
+        break
+      case '5': expense.type = 'Transporte'
+        break
+    }
+
+    line.insertCell(1).innerHTML = expense.type; //Tipo
+    line.insertCell(2).innerHTML = expense.description; //Descrição
+    line.insertCell(3).innerHTML = parseFloat(expense.value).toFixed(2); //Valor
+
+    // Criando botão de exclusão
+    let btnRemove = document.createElement('button');
+    btnRemove.innerText = 'X';
+    btnRemove.className = 'btn__remove';
+    btnRemove.id = `id__expense-${expense.id}`
+    line.insertCell(4).append(btnRemove);
+
+    btnRemove.onclick = function(){
+      let id = btnRemove.id.replace('id__expense-', '');
+
+      db.remove(id)
+
+      window.location.reload()
+    }
+
+  })
+}
+
